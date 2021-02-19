f26bfbaf00ab474923913c1a117a12eb1f68ffda
Kostenko <AKostenko@luxoft.com>
2021-02-19 10:23:06 +0300

    formats code

--- anagrams.js
+++ anagrams.js
@@ -1,20 +1,15 @@
-/**
-Напишите функцию, которая найдет все наборы анаграмм в строке. 
-Анаграммы - слова, составленные из одного и того же набора букв (рост-сорт-трос)
-*/
-
 const str = 'адрес карп кума куст мир мука парк рим среда стук рост сорт трос';
 
 function getAnagrams(str) {
   let b = [];
   let result = new Map();
   let arr = str.split(' ');
-  
+
   for (let i = 0; i < arr.length; i++) {
     let sort = arr[i].split('').sort().join('');
-    
+
     if (result.has(sort)) {
-      result.forEach((value,key)=>{
+      result.forEach((value, key) => {
         if (key === sort) {
           value.push(arr[i])
         }
