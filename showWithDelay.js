3a36146598b549b0258bd4140ac288dbbc3c105a
Kostenko <AKostenko@luxoft.com>
2021-02-19 10:15:20 +0300

    formats code

--- showWithDelay.js
+++ showWithDelay.js
@@ -1,19 +1,19 @@
 const ARR = [
-    {name: 'first', timeout: 3000},
-    {name: 'second', timeout: 5000},
-    {name: 'third', timeout: 2000},
+  { name: 'first', timeout: 3000 },
+  { name: 'second', timeout: 5000 },
+  { name: 'third', timeout: 2000 },
 ];
 // With loop
 function sleep(arr) {
   let i = 0;
   let delay = arr[0].timeout;
-  
+
   function loop() {
-      console.log(arr[i].name);
-      console.log(delay);
-      if (i++ < arr.length - 1) {
-          setTimeout(loop, delay += arr[i].timeout);
-      }
+    console.log(arr[i].name);
+    console.log(delay);
+    if (i++ < arr.length - 1) {
+      setTimeout(loop, delay += arr[i].timeout);
+    }
   }
   setTimeout(loop, delay);
 }
@@ -21,9 +21,9 @@ function sleep(arr) {
 function sleep2(array) {
   let delay = 0;
   array.forEach(function (el, i) {
-    setTimeout(function() {
-        console.log( array[i].name);
-      }, delay += array[i].timeout);
+    setTimeout(function () {
+      console.log(array[i].name);
+    }, delay += array[i].timeout);
   })
 }
 
